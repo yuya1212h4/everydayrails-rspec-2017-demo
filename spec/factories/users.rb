1fe94b8bc1de3943b4d5771a522c27c642341fea
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-03-16 09:54:26 +0900

    factoryfileの変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :user do
-    
+    first_name "Aaron"
+    last_name "Sumner"
+    email "tester@example.com"
+    password "password"
   end
 end
