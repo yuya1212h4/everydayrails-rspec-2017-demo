adb11eaa00746a4a790441ffa77b8f0bdc7cb788
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-03-16 10:18:05 +0900

    factoryを使うことによってのリファクタ

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,58 +4,37 @@ RSpec.describe User, type: :model do
   describe User do
 
     it "姓、名、メール、パスワードがあれば、有効な状態であること" do
-      user = User.new(
-        first_name: "Aaron",
-        last_name: "Sumner",
-        email: "test@example.com",
-        password: "password"
-      )
-      expect(user).to be_valid
+      expect(build(:user)).to be_valid
     end
 
     it "姓がなければ、無効な状態であること" do
-      user = User.new(last_name: nil)
+      user = build(:user, last_name: nil)
       user.valid?
       expect(user.errors[:last_name]).to include("can't be blank")
     end
 
     it "名がなければ、無効な状態であること" do
-      user = User.new(first_name: nil)
+      user = build(:user, first_name: nil)
       user.valid?
       expect(user.errors[:first_name]).to include("can't be blank")
     end
 
     it "メールアドレスがなければ、無効な状態であること" do
-      user = User.new(email: nil)
+      user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
     it "重複したメールアドレスなら、無効な状態であること" do
-      user = User.create(
-        first_name: "Aaron",
-        last_name: "Sumner",
-        email: "test@example.com",
-        password: "password"
-      )
-      user = User.new(
-        first_name: "Aaron",
-        last_name: "Sumner",
-        email: "test@example.com",
-        password: "password"
-      )
-      user.valid?
-      expect(user.errors[:email]).to include("has already been taken")
+      user = create(:user, email: "test@example.com")
+      other_user = build(:user, email: "test@example.com")
+      other_user.valid?
+      expect(other_user.errors[:email]).to include("has already been taken")
     end
 
     # nameメソッドのテスト
     it "ユーザーのフルネームを文字列として返すこと" do
-      user = User.create(
-        first_name: "Aaron",
-        last_name: "Sumner",
-        email: "test@example.com",
-        password: "password"
-      )
+      user = build(:user, first_name: "Aaron", last_name: "Sumner")
       expect(user.name).to eq("Aaron Sumner")
     end
 
