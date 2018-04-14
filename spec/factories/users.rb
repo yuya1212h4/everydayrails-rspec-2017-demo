9f80ad19b99bb649586157d77a8057e45bcc8f06
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-04-14 14:47:06 +0900

    factories/users.rbにaliasの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:owner] do
     first_name "Aaron"
     last_name "Sumner"
     sequence(:email) { |n| "tester#{n}@example.com" }
