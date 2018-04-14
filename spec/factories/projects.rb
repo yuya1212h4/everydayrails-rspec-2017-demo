a5af3b98fd6e9fc0fd5b695b9fcf9da61d1f5ac8
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-04-14 14:47:22 +0900

    project_specの追加

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,5 +4,28 @@ FactoryBot.define do
     description "A test project."
     due_on 1.week.from_now
     association :owner
+
+    # 昨日が締め切りのプロジェクト
+    factory :project_due_yesterday do
+      due_on 1.day.ago
+    end
+
+    # 今日が締切のプロジェクト
+    factory :project_due_today do
+      due_on Date.current.in_time_zone
+    end
+
+    # 明日が締め切りのプロジェクト
+    # factory :project_due_tomorrow do
+    #   due_on 1.day.from_now
+    # end
+
+    trait :due_tomorrow do
+      due_on 1.day.from_now
+    end
+
+    trait :with_notes do
+      after(:create){ |project| create_list(:note, 5, project: project) }
+    end
   end
 end
